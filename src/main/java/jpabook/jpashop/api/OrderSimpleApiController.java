7545a2310d79f7152755ad79a1f510d5edf1dd58
jinhee <jlee04@hubilon.com>
2021-06-30 16:06:00 +0900

    지연 로딩과 조회 성능 최적화 완료
    간단한 주문 조회 v3 : 엔터티를 DTO로 변환 - 페치 조인 최적화
    간단한 주문 조회 v4 : JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +31,7 @@ import static java.util.stream.Collectors.*;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
-
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
     /**
      * V1. 엔터티 직접 노출
      * - Hibernate5Module 모듈 등록, Lazy=null 처리
@@ -57,6 +59,18 @@ public class OrderSimpleApiController {
 
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream().map(o-> new SimpleOrderDto(o))
+                                .collect(Collectors.toList());
+
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4(){
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
     @Data
     static class SimpleOrderDto {
         private Long orderId;
